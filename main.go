632a65c1778e3682523c561d15dd1514600d7ebd
cooler-SAI <cooler19860212@yahoo.com>
2024-11-08 20:59:37 +0300

    set up simple universe program

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -11,5 +12,11 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	log.Info().Msg("Welcome to the Track-Tracker repo")
+
+	for {
+		log.Info().Msg("Hello all Here!")
+
+		time.Sleep(3 * time.Second)
+	}
 
 }
