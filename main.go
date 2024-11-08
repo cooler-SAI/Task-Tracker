af7153d7c7373d94194aa17788ed55a8f5079635
cooler-SAI <cooler19860212@yahoo.com>
2024-11-08 21:34:14 +0300

    added base new CLI app stuff

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 	"os"
 	"time"
 )
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+const taskFile = "tasks.json"
 
 func main() {
 
@@ -13,8 +25,20 @@ func main() {
 
 	log.Info().Msg("Welcome to the Track-Tracker repo")
 
+	rootCmd := &cobra.Command{
+		Use:   "task-cli",
+		Short: "Task Tracker CLI for managing your tasks",
+	}
+
+	rootCmd.AddCommand()
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	for {
-		log.Info().Msg("Hello all Here!")
+		log.Info().Msg("Scanning....")
 
 		time.Sleep(3 * time.Second)
 	}
