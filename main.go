59d44973537585ebee9df9823fa7ad1ef236775a
cooler-SAI <cooler19860212@yahoo.com>
2024-11-08 20:46:29 +0300

    added new track-tracker project to repo

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
 func main() {
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Welcome to the Track-Tracker repo")
 
 }
